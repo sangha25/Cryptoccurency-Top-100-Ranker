018c2157b5c4d50b2f5ce4d52ab9f3c54c8f883e
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 12:30:13 +0530

    Update crytpccurrency-top-100-ranker.py

--- crytpccurrency-top-100-ranker.py
+++ crytpccurrency-top-100-ranker.py
@@ -14,3 +14,18 @@ while nr_coins < len(listings_data):
     temp_ticker_api = ticker_api + str(nr_coins)
     ticker_data = requests.get(temp_ticker_api).json()
     ticker_data = ticker_data['data']
+    for coin in ticker_data:
+            name = ticker_data[coin]['name']
+            symbol = ticker_data[coin]['symbol']
+            coin = ticker_data[coin]['quotes']['USD']
+
+            table.add_row([ name,
+                            symbol,
+                            coin['price'],
+                            coin['volume_24h'],
+                            coin['market_cap'],
+                            coin['percent_change_1h'],
+                            coin['percent_change_24h'],
+                            coin['percent_change_7d']])
+
+        nr_coins += 100
