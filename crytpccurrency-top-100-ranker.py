b72ed428f341aff70f43b07548086b6bb1e42174
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 12:29:12 +0530

    Update crytpccurrency-top-100-ranker.py

--- crytpccurrency-top-100-ranker.py
+++ crytpccurrency-top-100-ranker.py
@@ -1,2 +1,5 @@
 import requests
 from prettytable import PrettyTable
+listings_api = 'https://api.coinmarketcap.com/v2/listings/'
+ticker_api = 'https://api.coinmarketcap.com/v2/ticker/?start='
+listings_data = requests.get(listings_api).json()['data']
